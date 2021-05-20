41a0ef5afa89ea14193663ba8b1ae8480fb69126
devipriyak <k.devipriya20@gmail.com>
2021-05-20 10:13:51 +0530

    Add files via upload

---
+++ BaseDemo.py
@@ -0,0 +1,32 @@
+
+a='10'
+
+
+a=int(a)#Default base is 10-Decimal number
+print(a)
+
+a='1111'
+a=int(a,base=2)#consider the given string is binary representation
+print('binary representation')
+print(a)#output:2
+
+
+
+print("Hex of 15",hex(15))#converts int to Hex value and the
+#result value is consider as string
+print("OCT of 7",oct(7))#converts int to oct
+print("bin of 7" ,bin(7))#converts int to binary
+
+#Number representations
+#Is there any possibility to convert from
+#integer to binary-2....
+#Integer to Octal-8
+#Integer to Hex-16
+
+
+
+
+
+
+
+
