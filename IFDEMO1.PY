ad5dcf37f0ce97fe67d2ab00ec2dcb19e77f335b
devipriyak <k.devipriya20@gmail.com>
2021-05-21 12:21:29 +0530

    Add files via upload

---
+++ IFDEMO1.PY
@@ -0,0 +1,19 @@
+#Control Structures
+'''
+CS determines the flow of exection
+'''
+''' why?The statements are executed in the sequential
+various types-multiline comment
+'''
+#if
+a=10 #READ INPUT FROM USER
+if a<20:  #if condition is true then the statements of if block is executed
+ print('a is <20')
+ print('HI')
+
+'''OUTPUT
+a is <20
+HI'''
+     
+      
+   
