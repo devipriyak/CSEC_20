aac2156277c96e1bf1227bdf0fc420a4254b96b8
devipriyak <k.devipriya20@gmail.com>
2021-06-24 10:08:42 +0530

    Add files via upload

---
+++ stringf-prefix.py
@@ -0,0 +1,14 @@
+#String
+s=raw_input("Enter Strings")
+print "size of string"
+size=len(s)
+#.format(var1,var2,var3....)
+print "String formatting using .format()"
+print "length of {} is {}" .format(s,size)
+#length of python csec is 11
+print "String formatting using %s"
+print "length of %s is %d" %(s,size)
+#print(f"length of {s} is {size}" )python 3.6
+#print(f"size of{s} is {size}"
+#syntax:print(f"{formatvaribale}{var2}")
+
