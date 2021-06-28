bea76e651099c83b853b8dbcc5db09761ce5f1c5
devipriyak <k.devipriya20@gmail.com>
2021-06-28 12:18:02 +0530

    Add files via upload

---
+++ listdemoCSEC.py
@@ -0,0 +1,13 @@
+#list creation-more than one value
+#branchlist-store list of branches offered by AEC
+branch_list=["CSE","IT","CIVIL","MECH","AGRI","PT","ECE","EEE"]
+print branch_list
+#get the type of brach_list
+print type(branch_list)
+#access first element from the branch_list
+
+print "Zero index ",branch_list[0]
+print " index 2" ,branch_list[2]#CIVIL
+#Get the size of List
+print "Size of List"
+print len(branch_list)
